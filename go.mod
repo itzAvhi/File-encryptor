9ff1d51452249389049c535d54f68a62f97208ba
Itz_Avhi <avhipro19@gmail.com>
2026-04-16 13:59:19 +0545

    Added key derivation logic

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module file_encryptor
 
 go 1.25.2
 
-require golang.org/x/crypto v0.50.0 // indirect
+require golang.org/x/crypto v0.50.0
--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
 	"flag"
 	"fmt"
 	"os"
+
+	"golang.org/x/crypto/pbkdf2"
 )
 
+func deriveKey(password string, salt []byte) []byte {
+	return pbkdf2.Key([]byte(password), salt, 1000000, 32, sha256.New)
+}
+
+func generateSalt() ([]byte, error) {
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	return salt, err
+}
+
 func main() {
-	// Define the flags
 	mode := flag.String("mode", "", "Operation mode: 'encrypt' or 'decrypt'")
 	filePath := flag.String("file", "", "Path to the file")
 	password := flag.String("pass", "", "Password for encryption/decryption")
 
 	flag.Parse()
 
-	// Validate inputs
 	if *mode == "" || *filePath == "" || *password == "" {
 		fmt.Println("Usage: -mode [encrypt/decrypt] -file [path] -pass [password]")
 		os.Exit(1)
@@ -24,10 +36,10 @@ func main() {
 	switch *mode {
 	case "encrypt":
 		fmt.Printf("Encrypting file: %s\n", *filePath)
-		// Call your encrypt function here
+
 	case "decrypt":
 		fmt.Printf("Decrypting file: %s\n", *filePath)
-		// Call your decrypt function here
+
 	default:
 		fmt.Println("Invalid mode. Use 'encrypt' or 'decrypt'.")
 	}
